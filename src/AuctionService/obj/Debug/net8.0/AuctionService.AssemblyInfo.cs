022843c67955b7ba09c9cf556b392ae2edd0a92f
Cleverton Teixeira <clevertons.b.t@gmail.com>
2024-05-27 00:40:35 -0300

    End of Section 3 SearchSection

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55803898328569102e9d4050b020dfb1d4f8d701")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
